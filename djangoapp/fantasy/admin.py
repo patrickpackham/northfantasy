d67b34a82bfd495c446661c2e8753efc53d28ad3
Pat Packham <pat@redcrowdigital.com.au>
2023-05-10 14:21:52 +1000

    Update django admin.

--- djangoapp/fantasy/admin.py
+++ djangoapp/fantasy/admin.py
@@ -1,34 +1,31 @@
 from django.contrib import admin
 from .models import *
 
-# TODO Flesh these out.
-
 
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['id', 'name', 'admin']
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'league', 'default_position']
 
 
 @admin.register(Round)
 class RoundAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'league', 'played']
 
 
 @admin.register(LeagueRule)
 class LeagueRule(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'number', 'league', 'midfield_points', 'forward_points', 'defender_points', 'keeper_points']
 
 
 @admin.register(PlayerPoints)
 class PlayerPointsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'rule__title', 'player__name', 'round__number', 'points']
 
 
 @admin.register(PlayerRoundPosition)
 class PlayerRoundPosition(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'player__name', 'round__number', 'position']
