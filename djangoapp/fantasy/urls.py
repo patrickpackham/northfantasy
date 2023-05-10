bff9fc72d7dd779824e68b2b7917f5a3f022b2f9
Pat Packham <pat@redcrowdigital.com.au>
2023-05-10 11:22:57 +1000

    Actually import static files.

--- djangoapp/fantasy/urls.py
+++ djangoapp/fantasy/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import (
     LeagueHome,
